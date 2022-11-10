a529ae4584d9e7ed156dd0c323e5b2c75f386b2b
kuks2309 <70010841+kuks2309@users.noreply.github.com>
2022-11-10 16:34:46 +0900

    Add files via upload

---
+++ 20221110/log/log/log.cpp
@@ -0,0 +1,42 @@
+﻿// log.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int a = 0;
+	int number_of_digits = 0;
+	int i;
+	int result  = 0; 
+	int reverse = 0;
+	scanf("%d", &a);
+
+	number_of_digits = log10(a);
+	printf("number_of_digits =  %d\n", number_of_digits);
+
+	for (i = number_of_digits; i >= 0; i--)
+	{
+		result = a / pow(10, i);
+		a = a - result* pow(10, i);
+		printf("%d %d\n",i,result);
+		reverse += result * pow(10, number_of_digits-i);
+	}
+
+	printf("%d \n", reverse);
+	return 0;
+}
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
